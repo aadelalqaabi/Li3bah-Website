91cf90ea16bc994fb8dd90a07271799cc9543426
adelalqaabi <adelalqaapi1998@gmail.com>
2025-02-27 14:44:30 +0300

    added audio

--- li3bah-website/src/stores/instance.js
+++ li3bah-website/src/stores/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const baseURL = "https://www.cotankw.com/api"; //Prod
-const baseURL = "http://localhost:8000/api"; //Dev
+const baseURL = "https://www.cotankw.com/api"; //Prod
+//const baseURL = "http://localhost:8000/api"; //Dev
 
 const instance = axios.create({
   baseURL: baseURL,
